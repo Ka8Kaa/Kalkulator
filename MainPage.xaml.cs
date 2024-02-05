1d208061d1155781689c61d228309ffaa87b1b7b
Ka8Kaa <133215009+Ka8Kaa@users.noreply.github.com>
2024-02-05 23:27:25 +0100

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Kalkulator
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void btn1_Click(object sender, RoutedEventArgs e)
+        {
+            // Zbrajanje
+            int zbroj = Convert.ToInt32(textBox1.Text) + Convert.ToInt32(textBox2.Text);
+            textBlock1.Text = zbroj.ToString();
+        }
+
+        private void btn2_Click(object sender, RoutedEventArgs e)
+        {
+            // Oduzimanje
+            int razlika = Convert.ToInt32(textBox1.Text) - Convert.ToInt32(textBox2.Text);
+            textBlock1.Text = razlika.ToString();
+        }
+
+        private void btn3_Click(object sender, RoutedEventArgs e)
+        {
+            // Množenje
+            int umnozak = Convert.ToInt32(textBox1.Text) * Convert.ToInt32(textBox2.Text);
+            textBlock1.Text = umnozak.ToString();
+        }
+
+        private void btn4_Click(object sender, RoutedEventArgs e)
+        {
+            // Dijeljenje
+            int kolicnik = Convert.ToInt32(textBox1.Text) / Convert.ToInt32(textBox2.Text);
+            textBlock1.Text = kolicnik.ToString();
+        }
+
+        private void Radio1_Checked(object sender, RoutedEventArgs e)
+        {
+            // DEC
+            textBlock1.Text = Convert.ToInt32(textBlock1.Text).ToString();
+        }
+
+        private void Radio2_Checked(object sender, RoutedEventArgs e)
+        {
+            // BIN
+            int decimalnaVrijednost = Convert.ToInt32(textBlock1.Text);
+            textBlock1.Text = Convert.ToString(decimalnaVrijednost, 2);
+        }
+
+        private void Radio3_Checked(object sender, RoutedEventArgs e)
+        {
+            // OCT
+            int decimalnaVrijednost = Convert.ToInt32(textBlock1.Text);
+            textBlock1.Text = Convert.ToString(decimalnaVrijednost, 8);
+        }
+
+        private void Radio4_Checked(object sender, RoutedEventArgs e)
+        {
+            // HEX
+            int decimalnaVrijednost = Convert.ToInt32(textBlock1.Text);
+            textBlock1.Text = Convert.ToString(decimalnaVrijednost, 16);
+        }
+
+
+
+
+    }
+}
